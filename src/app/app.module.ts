890041d34f7704dc9ccf8c9ab440323ea62b4534
Leandro <planasleandro@gmail.com>
2022-02-04 11:20:56 -0300

    Modulos container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component';
+import { LogrosComponent } from './componentes/logros/logros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    AptitudesComponent,
+    LogrosComponent
   ],
   imports: [
     BrowserModule,
